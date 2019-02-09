f44c57f295e69981b2850af9c8c5fc2d62197d0e
Matthew McLean <matthewmclean21@gmail.com>
2019-02-09 12:03:45 -0800

    Create unit test and card test files

---
+++ projects/mcleanm/dominion/unittest1.c
@@ -0,0 +1,30 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main()
+{
+    printf("Testing compare function.\n");
+
+    int higher = 1;
+    int lower = 0;
+    int same = 1;
+    int same1 = 1;
+
+    //Test for higher first value
+    assert(compare(&higher, &lower) == 1);
+    //Lower first value
+    assert(compare(&lower, &higher) == -1);
+    //Equal value
+    assert(compare(&same, &same1) == 0);
+    //Same variable (also equal value)
+    assert(compare(&same, &same) == 0);
+
+    printf("Compare test passed\n");
+
+    return 0;
+}
---
+++ projects/mcleanm/dominion/unittest2.c
@@ -0,0 +1,8 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
