ec9e8ed8e59d7afd4cb06ff924b95350b99d6b54
Matthew McLean <matthewmclean21@gmail.com>
2019-02-28 20:58:23 -0800

    Implement start of adventurer implementation of random test

--- projects/mcleanm/dominion/randomtestadventurer.c
+++ projects/mcleanm/dominion/randomtestadventurer.c
@@ -3,22 +3,46 @@
 #include <stdlib.h> 
 #include <time.h> 
 #include "dominion.h"
+#include "testHelpers.h"
+
+#define TRUE 1
+#define FALSE 0
+
+void printTestResult(int failed)
+{
+    if(failed == TRUE)
+    {
+        printf("Test failed.");
+    }
+}
 
 int main() 
 {
     srand(time(0));
-    struct ResultPair* testPairs[NUMBER_TESTS];
-    int i = 0;
-    for(i = 0; i < NUMBER_TESTS; i++)
-    {
-        generateResultPair(testPairs[i]);
-    }
+    struct gameState* state = NULL;
 
-    //Generate random state, add to 
+    // Randomize the game state
+    state = getRandomState(state, adventurer);
 
-    //Play adventurer
+    // Play the adventurer, print if successful
+    printTestResult(playAdventurer(state, state->whoseTurn));
 
-    //Assert state changed correctly
+    // // Test each game state
+    // int i;
+    // for(i = 0; i < NUMBER_TESTS; i++)
+    // {
+    //     // Randomize the game state
+    //     if(state != NULL)
+    //     {
+    //         free(state);
+    //         state = NULL;
+    //     }
+    //     state = getRandomState(state, adventurer);
 
+    //     // Play the adventurer, print if successful
+    //     printTestResult(playAdventurer(state, state->whoseTurn));
+    // }
+
+    free(state);
     return 0;
 }
